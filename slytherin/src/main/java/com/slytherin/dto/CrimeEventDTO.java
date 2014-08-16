14ee81ae1b7ef68e02de4cbd7fc616c5a0b90af8
kdmandawe <kdmandawe06@live.com>
2014-08-16 21:21:26 +0800

    crimeEvent, address

---
+++ slytherin/src/main/java/com/slytherin/dto/CrimeEventDTO.java
@@ -0,0 +1,60 @@
+package com.slytherin.dto;
+
+import java.util.Date;
+
+public class CrimeEventDTO {
+    
+    private String id;
+    private String crimeType;
+    private String title;
+    private String details;
+    private Date reportDate;
+    private AddressDTO crimeLocation;
+    private AddressDTO userLocation;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getCrimeType() {
+        return crimeType;
+    }
+    public void setCrimeType(String crimeType) {
+        this.crimeType = crimeType;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public Date getReportDate() {
+        return reportDate;
+    }
+    public void setReportDate(Date reportDate) {
+        this.reportDate = reportDate;
+    }
+    public AddressDTO getCrimeLocation() {
+        return crimeLocation;
+    }
+    public void setCrimeLocation(AddressDTO crimeLocation) {
+        this.crimeLocation = crimeLocation;
+    }
+    public AddressDTO getUserLocation() {
+        return userLocation;
+    }
+    public void setUserLocation(AddressDTO userLocation) {
+        this.userLocation = userLocation;
+    }
+    
+    
+    
+}
